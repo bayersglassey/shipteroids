ed2a73ff223f5ed89642f49f85ed532006e06c27
Ben Ayers-Glassey <bayersglassey@gmail.com>
2021-08-17 23:50:48 -0700

    Added recs/2player.gif

--- rec-byzanz.sh
+++ rec-byzanz.sh
@@ -1,12 +1,24 @@
 
 set -euo pipefail
 
-outfile="${1:-out.gif}"
+outfile=out.gif
+
+players="${1:-1}"
+echo "Setting up for $players players..."
 
 w=700
 h=700
 x=439
 y=142
+
+if test "$players" = 2
+then
+    w=1275
+    h=600
+    x=100
+    y=182
+fi
+
 delay=3
 
 echo "Recording to file $outfile in $delay seconds..." >&2
